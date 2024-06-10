dc6c20dd5e8923145e9f9b772c3f50479c25f7a2
Kavinda <rdks.kavinda@gmail.com>
2024-06-10 13:50:14 +0530

    My first test case:title check

--- src/test/java/day20/FirstTestCase.java
+++ src/test/java/day20/FirstTestCase.java
@@ -1,7 +1,43 @@
 package day20;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+Test Case
+
+1.launch browser
+2.open URL "https://demo.opencart.com/"
+3.Validate title should be "Your Store"
+4.close browser
+
+*/
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTestCase {
     public static void main(String[] args){
+//        1.launch browser
+//        ChromeDriver driver=new ChromeDriver();
+          WebDriver driver=new ChromeDriver(); //upCasting
+
+//        2.open URL "https://demo.opencart.com/"
+        driver.get("https://demo.opencart.com/");
+
+//        3.Validate title should be "Your Store"
+       String actTitle=driver.getTitle();
+       if(actTitle.equals("Your Store")){
+           System.out.println("Test Case Passed");
+       }else{
+           System.out.println("Test failed");
+       }
+
+//        4.close browser
+//         driver.close();
+         driver.quit();
+
+
+
 
     }
 }
